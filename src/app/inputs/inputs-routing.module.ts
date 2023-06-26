87acbd872c18c4c6ad94c3d6b5db324ec06852ec
Juan Esteban Bedoya <juanes2293@hotmail.com>
2023-06-25 20:40:43 -0500

    Subir proyecto

--- src/app/inputs/inputs-routing.module.ts
+++ src/app/inputs/inputs-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputComponent } from './input/input.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./inputs/inputs.module').then(m => m.InputsModule)
+    component: InputComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class InputsRoutingModule { }
